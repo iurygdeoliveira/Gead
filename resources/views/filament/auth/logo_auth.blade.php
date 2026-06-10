650feb4feeea06fc8757d8828f6b7077c33a54e5
Iury Gomes <iurygdeoliveira@gmail.com>
2026-06-10 13:12:24 -0300

    style: use GeAD logo with drop shadow contour on auth login page

--- resources/views/filament/auth/logo_auth.blade.php
+++ resources/views/filament/auth/logo_auth.blade.php
@@ -1,4 +1,4 @@
-<img src="{{ asset('images/labsis_logo_bg.png') }}" alt="LabSIS" class="h-full w-auto object-contain" />
+<img src="{{ asset('images/logo.png') }}" alt="GeAD" class="h-full w-auto object-contain" style="filter: drop-shadow(0 0 12px oklch(1 0 0 / 0.95)) drop-shadow(0 0 32px oklch(1 0 0 / 0.75)) drop-shadow(0 0 60px oklch(1 0 0 / 0.35)) drop-shadow(0 4px 16px oklch(0 0 0 / 0.65));" />
 
 
 
